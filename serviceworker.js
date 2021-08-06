761159f07725a65b7831c58f6b534235541b2448
dk-raw <ddkatsios@protonmail.com>
2021-08-06 08:45:28 +0300

    minor changes

--- serviceworker.js
+++ serviceworker.js
@@ -16,7 +16,7 @@ self.addEventListener('fetch', (event) => {
         caches.match(event.request)
             .then(() => {
                 return fetch(event.request)
-                    .catch(() => caches.match('index.html'))
+                    .catch(() => caches.match('index.html'));
             })
     )
 });
